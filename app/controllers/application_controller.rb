83e20f4cd48be33793950941503617fed3d11b72
mugineko28 <foradamstories@yahoo.co.jp>
2021-01-04 12:12:11 +0900

    ビューの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
+  # before_action :basic_auth
 
 
-  private
+  # private
 
-  def basic_auth
-    authenticare_or_request_with_http_basic do |username, password|
-      username == 'hinomoto' && password == '1896'
-    end
-  end
+  # def basic_auth
+  #   authenticare_or_request_with_http_basic do |username, password|
+  #     username == 'hinomoto' && password == '1896'
+  #   end
+  # end
 end
