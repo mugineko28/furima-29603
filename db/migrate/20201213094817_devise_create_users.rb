913ef58b3a719810580710aa13da923dbb2c0b1e
mugineko28 <foradamstories@yahoo.co.jp>
2021-01-24 14:52:02 +0900

    ログイン、新規登録機能のトップ表示

--- db/migrate/20201213094817_devise_create_users.rb
+++ db/migrate/20201213094817_devise_create_users.rb
@@ -6,7 +6,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
 
       t.string :name,               null: false, default: ""
-      t.string :email,              null: false, default: ""
+      t.string :email,              null: false,unique:true, default: ""
       t.string :encrypted_password, null: false, default: ""
       t.string :last_name,          null: false, default: ""
       t.string :first_name,         null: false, default: ""
@@ -15,7 +15,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.date :birthday,             null: false
     
 
-      ## Recoverable
+      # Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
 
