3397f1aa94a540533ea8c23ce3fe38e959e711b6
mugineko28 <foradamstories@yahoo.co.jp>
2021-01-24 16:37:21 +0900

    ユーザ登録機能（修正）

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  
   private
 
     def basic_auth
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
 
    def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, 
-      keys: [:name, 
+      keys: [:nickname, 
         :email, 
         :encrypted_password, 
         :last_name, 
--- db/migrate/20201213094817_devise_create_users.rb
+++ db/migrate/20201213094817_devise_create_users.rb
@@ -5,7 +5,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
     create_table :users do |t|
       ## Database authenticatable
 
-      t.string :name,               null: false, default: ""
+      t.string :nickname,               null: false, default: ""
       t.string :email,              null: false,unique:true, default: ""
       t.string :encrypted_password, null: false, default: ""
       t.string :last_name,          null: false, default: ""
