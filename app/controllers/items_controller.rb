22cd8c1a24e281d60af62cae31c5b628f8e7f14c
mugineko28 <foradamstories@yahoo.co.jp>
2021-01-31 16:19:47 +0900

    モデルの作成と付随する記述の追記

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,22 @@
 class ItemsController < ApplicationController
 
 def index
-
+  @items = Item.all
 end
+
+def new
+  @items = Item.new
+end
+
+def create
+  Item.create(item_params)
+end
+
+private
+  def item_params
+    params.require(:item).
+    permit(:user,:name, :image, :details, :price, 
+           :category_id, :status_id, :shipping_charges_id, :prefecture_id, :day_to_skup_id)
+  end
 
 end
