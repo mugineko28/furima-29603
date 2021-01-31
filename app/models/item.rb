a9effcbb2be375c042efaa7f8227cf1e8791ef6e
mugineko28 <foradamstories@yahoo.co.jp>
2021-01-31 18:04:47 +0900

    itemモデルのバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,16 @@
 class Item < ApplicationRecord
-
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  
+  with_options presence: true do
+  validates :user
+  validates :name
+  validates :details
+  validates :price
+  validates :category_id
+  validates :status_id
+  validates :shipping_charges_id
+  validates :day_to_skup_id
+  end
   
 end
