7fc626170d7e2f872dd835eca0a21cb0d51fe3e8
mugineko28 <foradamstories@yahoo.co.jp>
2021-02-01 21:20:13 +0900

    記述ミスの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,11 +12,11 @@ def create
   Item.create(item_params)
 end
 
-private
+
+ private
   def item_params
     params.require(:item).
     permit(:user,:name, :image, :details, :price, 
-           :category_id, :status_id, :shipping_charges_id, :prefecture_id, :day_to_skup_id)
+           :category_id, :status_id, :shipping_charges_id, :prefecture_id, :day_to_skip_id)
   end
-
 end
