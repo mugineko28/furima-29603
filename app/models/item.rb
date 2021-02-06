84411a2cfcc44a2266944f5fec52f3175a33d8c0
mugineko28 <foradamstories@yahoo.co.jp>
2021-02-06 19:01:03 +0900

    商品の詳細と配送についてのプルダウンバーの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,5 +5,6 @@ class Item < ApplicationRecord
     validates :user, :name,:details,:price,
               :category_id,:status_id,:shipping_charges_id,
               :prefecture_id,:day_to_skip_id, presence: true
-    validates :genre_id, numericality: { other_than: 1 } 
+    validates :category_id,:status_id, :spping_charges, :prefeture,:days_to_skip,
+              numericality: { other_than: 1 } 
   end
