dbc92dec3148e30d78cbb1dd5cbac2e6b8edc250
mugineko28 <foradamstories@yahoo.co.jp>
2021-02-07 16:39:24 +0900

    プルダウンバーの実装の修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,6 +5,15 @@ class Item < ApplicationRecord
     validates :user, :name,:details,:price,
               :category_id,:status_id,:shipping_charges_id,
               :prefecture_id,:day_to_skip_id, presence: true
-    validates :category_id,:status_id, :spping_charges, :prefeture,:days_to_skip,
-              numericality: { other_than: 1 } 
+
+
+    with options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :status_id
+      validates :shipping_charges
+      validates :prefeture
+      validates :days_to_skip
+    end
+    validates_inclusion_of :price, in: 300..9_999_999
+
   end
