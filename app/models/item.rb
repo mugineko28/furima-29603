728c444504054c0ba63e78e4c75530cf4a9d01f2
mugineko28 <foradamstories@yahoo.co.jp>
2021-02-14 21:33:39 +0900

    出品機能の単体テストコードの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,14 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_charges
+  belongs_to :prefecture
+  belongs_to :day_to_skip
+  belongs_to :user
   has_one_attached :image
   
-    validates :name,:details,:price,
+    validates :name,:details,:price,:image,
               :category_id,:status_id,:shipping_charges_id,
               :prefecture_id,:day_to_skip_id, presence: true
 
@@ -15,6 +20,6 @@ class Item < ApplicationRecord
       validates :prefecture_id
       validates :day_to_skip_id
     end
-    validates_inclusion_of :price, in: 300..9_999_999
-
+    validates :price, numericality: { with: /\A[0-9]+\z/, message:"Half-width number"}
+    validates_inclusion_of :price, in:(300..9_999_999), message: "Out of setting range" 
   end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,14 +1,93 @@
 require 'rails_helper'
-
-RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
-
-describe '商品出品機能' do
-  context '出品がうまくいくとき' do
-    it "nameとimageとprice、category_id, status_id, 
-       shipping_charges_id, prefecture_id, day_to_skip_idがそれぞれ存在する" do
-    expect(@items).to be_valid
+RSpec.describe Item do
+  before do
+    @item = FactoryBot.build(:item)
+  
   end
 
+ describe '商品出品機能' do
+  context '出品がうまくいくとき' do
+    it "nameとimageとprice、category_id, status_id, 
+       shipping_charges_id, prefecture_id, day_to_skip_idがあれば出品できる" do
+      expect(@item).to be_valid
+    end
+  end
+  context '商品出品がうまくいかないとき' do
+    it "nameがないと出品できない" do
+      @item.name = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Name can't be blank")
+    end
+
+    it "imageがないと出品できない" do
+      @item.image = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Image can't be blank")
+    end
+
+    it "detailsがないと出品できない" do
+      @item.details = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Details can't be blank")
+    end
+
+    it "priceがないと出品できない" do
+      @item.price = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price can't be blank")
+    end
+
+    it "priceが全角だと出品できない" do
+      @item.price = "1００００"
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price Half-width number")
+    end
+
+    it "priceが299円以下だと出品できない" do
+      @item.price = 299
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price Out of setting range")
+    end
+
+    it "priceが10,000,000円以上だと出品できない" do
+      @item.price = 10_000_000
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price Out of setting range")
+    end
+
+
+    it "category_idが選択されていないければ出品できない" do
+      @item.category_id = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Category can't be blank")
+    end
+
+    it "status_idが選択されていなければ出品できない" do
+      @item.status_id = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Status can't be blank")
+    end
+
+    it "shipping_charges_idが選択されていなければ出品できない" do
+      @item.shipping_charges_id = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Shipping charges is not a number")
+    end
+
+    it "prefecture_idが選択されていなければ出品できない" do
+      @item.prefecture_id = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+    end
+
+    it "day_to_skip_idが選択されていなければ出品できない" do
+      @item.day_to_skip_id = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Day to skip can't be blank")
+    end
+
+
+  end
+
+   end
 end
