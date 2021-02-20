e54f237b15085adfac2798469ae01465060429d5
mugineko28 <foradamstories@yahoo.co.jp>
2021-02-20 12:31:03 +0900

    バリデーション の設定

--- db/migrate/20210218053036_create_addresses.rb
+++ db/migrate/20210218053036_create_addresses.rb
@@ -3,6 +3,13 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
     create_table :addresses do |t|
 
       t.timestamps
+      t.string         :street_address,               null:false
+      t.integer        :prefecture_id,                null:false
+      t.string         :postal_code,                  null:false
+      t.string         :municipality,                 null:false
+      t.string         :building_name
+      t.string         :phone_number,                 null:false
+      t.references      :purchase_information,         null:false, foreign_key: true
     end
   end
 end
