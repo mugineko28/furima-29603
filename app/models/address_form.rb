1e4e548ec1457b8b158536bd7a6fe922385cbf1c
mugineko28 <foradamstories@yahoo.co.jp>
2021-02-23 17:47:08 +0900

    バリデーションの設定

--- app/models/address_form.rb
+++ app/models/address_form.rb
@@ -1,26 +1,26 @@
 class AddressForm
   include ActiveModel::Model
   attr_accessor :street_address, :prefecture_id, :postal_code, 
-                :municipality, :building_name, :phone_number,
-                :purchase_information
+                :municipality, :building_name, :phone_number, :user_id, :item_id
 
     with_options presence: true do
-     validates :user
-     validates :item
-     validates :street_address, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
-     validates :prefecture_id
-     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
-     validates :municipality,  format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
-     validates :building_name
+     validates :user_id
+     validates :item_id
+     validates :street_address, format: {with:/\A[ぁ-んァ-ン一-龥々]/, message: "Can't be blank"}
+     validates :postal_code, format: { with:/\A\d{3}[-]\d{4}\z/ , message: "is invalid. Include hyphen(-)" }
+     validates :municipality,  format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: "is invalid"}
      validates :phone_number, format: { with: /\A[0-9]+\z/ , message: "phone number can't be blank"}
-     validates :purchase_information
     end
 
-  def save
+    with_options numericality: { other_than: 1 } do
+      validates :prefecture_id 
+    end
 
+
+  def save
+   purchase_information = PurchaseInformation.create(user_id: user_id, item_id: item_id)
     Address.create(street_address: street_address, prefecture_id: prefecture_id, 
                    postal_code: postal_code, municipality: municipality, building_name: building_name, 
-                   phone_number: phone_number, purchase_information:purchase_information )
-    PurchaseInformation.create(user: user, item: item)
+                   phone_number: phone_number, purchase_information_id: purchase_information.id )
   end
 end
