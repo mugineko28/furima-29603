2b72b42b94b2d6e4fc3694f9a2b3dea10b348eea
mugineko28 <foradamstories@yahoo.co.jp>
2021-03-02 12:29:59 +0900

    住所のFakerの追加

---
+++ spec/factories/address_forms.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :address_form do
+    token                             {'tok_1234567'}
+    postal_code                       {'123-4567'}
+    municipality                      {'赤崎市青海町'}
+    street_address                    {'緑通1-1-1'}
+    phone_number                      {'12345678912'}
+    prefecture_id                     {4}
+  end
+end
