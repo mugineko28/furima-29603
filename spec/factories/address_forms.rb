b97ba10c280d8b1c3e381c0bc06d06448ce0aa90
mugineko28 <foradamstories@yahoo.co.jp>
2021-03-03 11:46:38 +0900

    建物名のFactoryBotの追加

--- spec/factories/address_forms.rb
+++ spec/factories/address_forms.rb
@@ -4,6 +4,7 @@ FactoryBot.define do
     postal_code                       {'123-4567'}
     municipality                      {'赤崎市青海町'}
     street_address                    {'緑通1-1-1'}
+    building_name                     {'白ビル111'}
     phone_number                      {'12345678912'}
     prefecture_id                     {4}
   end
